94ae68f5139259e9695d8c6db179f5f0de93f51f
Shien Foo <shien.foo@sparkworks.co.id>
2023-02-09 06:28:54 +0700

    Initial Commit

---
+++ testcicd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testcicd
+//
+//  Created by Shien Foo on 9/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
